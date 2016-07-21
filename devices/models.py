f918c8a554749d326a9bbd4f65fe1832842c0351
Jacob Henson <jhenson@camber.com>
2016-07-21 12:33:45 -0500

    updated device model

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,49 @@
+"""
+Devices Models
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    """
+    Manufacturer Model
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=30)
+
+
+class Device(models.Model):
+    """
+    Device Model
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    hostname = models.CharField(max_length=30)
+    ipaddress = models.CharField(max_length=30)
+    operating_system = models.CharField(max_length=30)
+    virtualized = models.BooleanField(default=False)
+    model = models.CharField(max_length=30)
+    users = models.CharField(max_length=30)
+
+
+class MAC(models.Model):
+    """
+    MAC Model
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    address = models.CharField(max_length=30)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+
+class Ip(models.Model):
+    """
+    An history of IPs associated with a Device
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    address = models.GenericIPAddressField()
+    mac = models.ForeignKey(MAC, on_delete=models.CASCADE)
