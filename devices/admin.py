270507e63f66056ce87e7823ec9bc3425b9438f0
Jacob Henson <jhenson@camber.com>
2016-08-19 15:56:49 -0500

    model tweaks

--- devices/admin.py
+++ devices/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Manufacturer
-from .models import Device
+from .models import Manufacturer,Device,MAC,Ip
+
 
 admin.site.register(Manufacturer)
 admin.site.register(Device)
+admin.site.register(MAC)
+admin.site.register(Ip)
--- devices/models.py
+++ devices/models.py
@@ -23,14 +23,13 @@ class Device(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    hostname = models.CharField(max_length=30)
-    ipaddress = models.CharField(max_length=30)
-    operating_system = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, blank=True)
+    operating_system = models.CharField(max_length=30, blank=True)
     virtualized = models.BooleanField(default=False)
-    model = models.CharField(max_length=30)
-    users = models.CharField(max_length=30)
+    model = models.CharField(max_length=30, blank=True)
+    users = models.CharField(max_length=30, blank=True)
     def __str__(self):
-        return self.hostname[:50]
+        return self.name[:50]
 
 
 class MAC(models.Model):
@@ -41,6 +40,8 @@ class MAC(models.Model):
     modified = models.DateTimeField(auto_now=True)
     address = models.CharField(max_length=30)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.address
 
 
 class Ip(models.Model):
@@ -51,3 +52,15 @@ class Ip(models.Model):
     modified = models.DateTimeField(auto_now=True)
     address = models.GenericIPAddressField()
     mac = models.ForeignKey(MAC, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.address
+
+class Hostname(models.Model):
+    """docstring for hostname"""
+    name = models.CharField(max_length=30, blank=True)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+        
